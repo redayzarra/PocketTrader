fc7390cbfeb1a656685fbd69cd31560596025e07
ReDay Zarra <redayzarra@gmail.com>
2023-05-01 08:33:12 -0400

    Logger produces file names based on time

--- logger.py
+++ logger.py
@@ -1,13 +1,17 @@
 import logging
 import os
+from datetime import datetime
 
 # Create a logs directory if it doesn't already exist
-path = "./logs"
-os.makedirs(path, exist_ok=True)
-print(f"Directory '{path}' created or already exists.")
+log_path = "./logs"
+os.makedirs(log_path, exist_ok=True)
+print(f"Directory '{log_path}' created or already exists.")
 
 # Configure logging
-log_file = os.path.join(path, "trading_bot.log")
+log_name = datetime.now().strftime(
+    "%Y-%m-%d_%H-%M-%S_trading_bot.log")  # Changed format
+log_file = os.path.join(log_path, log_name)
+
 logging.basicConfig(filename=log_file,
                     format="%(asctime)s - %(levelname)s: %(message)s",
                     encoding="utf-8",
