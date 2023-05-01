3482aa1c580ed76c4df1cd06b8022bd74d8c5637
ReDay Zarra <redayzarra@gmail.com>
2023-05-01 08:05:32 -0400

    Logger for debugging works and formatting is correct

--- logger.py
+++ logger.py
@@ -1,7 +1,11 @@
 import logging
 
+# Creating log file to store errors
 logging.basicConfig(filename="trading_bot.log",
                     format="%(asctime)s - %(levelname)s: %(message)s",
-                    encoding="utf-8")
+                    encoding="utf-8",
+                    level=logging.DEBUG)
 
+logging.debug("Testing")
 
+logging.debug("Again, testing 2")
