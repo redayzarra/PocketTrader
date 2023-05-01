6cb7daa9d86da61ec8bdd8e02cc78ca25d2034c4
ReDay Zarra <redayzarra@gmail.com>
2023-05-01 08:19:23 -0400

    Folder for logging works

--- logger.py
+++ logger.py
@@ -1,6 +1,15 @@
 import logging
+import os
 
-# Creating log file to store errors
+path = "./logs"
+try:
+    os.mkdir(path)
+except OSError:
+    print("Creating directory %s not successful unless already exists" % path)
+else:
+    print("Directory created!")
+
+    # Creating log file to store errors
 logging.basicConfig(filename="trading_bot.log",
                     format="%(asctime)s - %(levelname)s: %(message)s",
                     encoding="utf-8",
@@ -13,3 +22,5 @@ logging.info("Useful messages")
 logging.warning("Warn me")
 
 logging.error("Any error messages")
+
+logging.critical("what's wrong")
