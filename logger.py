d0facfff8395be3ca43c5b96d3a008f8511fd416
ReDay Zarra <redayzarra@gmail.com>
2023-05-01 08:41:32 -0400

    Adding stream handler so log messages are also printed to the console

--- logger.py
+++ logger.py
@@ -9,13 +9,15 @@ print(f"Directory '{log_path}' created or already exists.")
 
 # Configure logging
 log_name = datetime.now().strftime(
-    "%Y-%m-%d_%H-%M-%S_trading_bot.log")  # Changed format
+    "%Y-%m-%d_%H-%M-%S.log")  # Changed format
 log_file = os.path.join(log_path, log_name)
 
 logging.basicConfig(filename=log_file,
                     format="%(asctime)s - %(levelname)s: %(message)s",
                     encoding="utf-8",
                     level=logging.DEBUG)
+
+logging.getLogger().addHandler(logging.StreamHandler())
 
 # Sample log messages
 logging.debug("Debugging message")
