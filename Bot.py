1a30b7ae6e28357b52de453988b57cad4e9bfedc
ReDay Zarra <redayzarra@gmail.com>
2023-05-02 08:01:52 -0400

    Adding get_ticker function to get user input and building main function

--- Bot.py
+++ Bot.py
@@ -4,9 +4,6 @@ from library import *
 from logger import *
 
 
-initialize_logging(log_level=logging.INFO, log_folder="logs")
-
-
 def account_verification():
     """
     Verifies the account and logs an error message if an exception occurs.
@@ -31,3 +28,37 @@ def clear_orders(open_orders):
         logging.info(f"Closing order {order.id}")
 
     logging.info("Clearing all open orders.")
+
+
+def get_ticker():
+    """
+    Prompt the user to enter a ticker symbol and return it.
+
+    Returns:
+        str: The entered ticker symbol.
+    """
+    ticker = input("What ticker do you want to operate with? Ticker: ")
+    return ticker
+
+
+def main():
+    """
+    Main function to:
+        - Initialize logging
+        - Verify the account
+        - Clear orders
+        - Get the user's desired ticker.
+    """
+    initialize_logging()
+    account_verification()
+
+    # Example open_orders list for demonstration
+    open_orders = []
+    clear_orders(open_orders)
+
+    ticker = get_ticker()
+    print(f"Operating with ticker: {ticker}")
+
+
+if __name__ == "__main__":
+    main()
