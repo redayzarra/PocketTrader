a9b7eead14c1c603f86d7cbc5e1f6a71545f4eb2
ReDay Zarra <redayzarra@gmail.com>
2023-05-02 07:37:50 -0400

    Converting logger into usable function

---
+++ Bot.py
@@ -0,0 +1,6 @@
+# Import all libraries
+from library import *
+from logger import *
+
+
+initialize_logging(log_level=logging.INFO, log_folder="custom_logs")
--- logger.py
+++ logger.py
@@ -2,26 +2,29 @@ import logging
 import os
 from datetime import datetime
 
-# Create a logs directory if it doesn't already exist
-log_path = "./logs"
-os.makedirs(log_path, exist_ok=True)
-print(f"Directory '{log_path}' created or already exists.")
 
-# Configure logging
-log_name = datetime.now().strftime(
-    "%Y-%m-%d_%H-%M-%S.log")  # Changed format
-log_file = os.path.join(log_path, log_name)
+def initialize_logging(log_level=logging.DEBUG, log_folder="logs"):
+    # Create a logs directory if it doesn't already exist
+    log_path = os.path.join(".", log_folder)
+    os.makedirs(log_path, exist_ok=True)
+    print(f"Directory '{log_path}' created or already exists.")
 
-logging.basicConfig(filename=log_file,
-                    format="%(asctime)s - %(levelname)s: %(message)s",
-                    encoding="utf-8",
-                    level=logging.DEBUG)
+    # Configure logging
+    log_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")
+    log_file = os.path.join(log_path, log_name)
 
-logging.getLogger().addHandler(logging.StreamHandler())
+    logging.basicConfig(
+        filename=log_file,
+        format="%(asctime)s - %(levelname)s: %(message)s",
+        encoding="utf-8",
+        level=log_level,
+    )
 
-# Sample log messages
-logging.debug("Debugging message")
-logging.info("Informational message")
-logging.warning("Warning message")
-logging.error("Error message")
-logging.critical("Critical message")
+    logging.getLogger().addHandler(logging.StreamHandler())
+
+    # Sample log messages
+    logging.debug("Debugging message")
+    logging.info("Informational message")
+    logging.warning("Warning message")
+    logging.error("Error message")
+    logging.critical("Critical message")
