08f21a09c55aefb1dcf70d3489200e3213ab1046
ReDay Zarra <redayzarra@gmail.com>
2023-05-07 06:11:54 -0400

    Creating PocketTrader

---
+++ PocketTrader.py
@@ -0,0 +1,16 @@
+# Import necessary libraries
+import math
+import os
+import sys
+import time
+from datetime import datetime
+
+# Import fun libraries
+import numpy as np
+import pandas as pd
+import pytz
+import tulipy as ti
+import yfinance as yf # Not needed
+import alpaca
+
+from logger import *
