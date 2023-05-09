684ef5044e38eff65f55d6688c1f1c6edcabdc3d
ReDay Zarra <redayzarra@gmail.com>
2023-05-09 09:08:18 -0400

    Building trader bot again

--- PocketTrader.py
+++ PocketTrader.py
@@ -2,8 +2,9 @@
 import math
 import os
 import sys
+from enum import Enum
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Import fun libraries
 import numpy as np
@@ -15,3 +16,16 @@ import alpaca
 
 from logger import *
 
+
+class Trader:
+    def __init__(self, ticker, api):
+        """
+        Initialize the Trader class with the given ticker and API.
+
+        Args:
+            ticker (str): The ticker symbol for the stock.
+            api: The API to be used for trading.
+        """
+        logging.info(f"Trader initialized with ticker {ticker}")
+        self.ticker = ticker
+        self.api = api
