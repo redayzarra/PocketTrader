83a8d69a66fd76cc59cf18deb0d8cee0c0421da5
ReDay Zarra <redayzarra@gmail.com>
2023-05-09 09:19:40 -0400

    Importing config with variables and API key, and creating set stoploss function

--- PocketTrader.py
+++ PocketTrader.py
@@ -15,16 +15,17 @@ import yfinance as yf  # Not needed
 import alpaca
 
 from logger import *
+import config
 
 
 class Trader:
     def __init__(self, ticker, api):
         """
-        Initialize the Trader class with the given ticker and API.
+        Initialize the Trader class with the given ticker and API key.
 
         Args:
             ticker (str): The ticker symbol for the stock.
-            api: The API to be used for trading.
+            api: The API key to be used for trading.
         """
         logging.info(f"Trader initialized with ticker {ticker}")
         self.ticker = ticker
@@ -50,3 +51,32 @@ class Trader:
         except:
             logging.error(f"{ticker} is encountering some issues")
             return False
+
+    def set_stoploss(self, entry_price, trend):
+        """
+        Calculate the stop loss based on the entry price and trend.
+
+        Args:
+            entry_price (float): The entry price of the stock.
+            trend (str): The trade direction ("long" or "short").
+
+        Returns:
+            float: The stop loss price.
+
+        Raises:
+            ValueError: If an invalid trend is provided.
+        """
+        try:
+            if trend == "long":
+                stop_loss = entry_price - (entry_price * config.stopLossMargin)
+                logging.info(f"Stop loss set for long at {stop_loss:.2f}")
+                return stop_loss
+            elif trend == "short":
+                stop_loss = entry_price + (entry_price * config.stopLossMargin)
+                logging.info(f"Stop loss set for short at {stop_loss:.2f}")
+                return stop_loss
+            else:
+                raise ValueError
+        except Exception as e:
+            logging.error(f"The trend value doesn't make sense: {trend}")
+            sys.exit()
