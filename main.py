30d10fa4315dc6230505f271f8d16b70384f86aa
ReDay Zarra <redayzarra@gmail.com>
2023-05-10 06:02:36 -0400

    Adding structure to the main file

--- main.py
+++ main.py
@@ -1,13 +1,78 @@
-from config import API_KEY, SECRET_KEY
-from PocketTrader import TraderBot
-import Strategies
+# encoding: utf-8
+import sys
+import time
+import config
+from PocketTrader import Trader
+from logger import *
+import alpaca as tradeapi
+
+
+def check_account_status(api):
+    try:
+        account = api.get_account()
+        if account.status != "ACTIVE":
+            logging.info("The account is not ACTIVE, aborting")
+            sys.exit()
+    except Exception as e:
+        logging.error("Could not get account info, aborting")
+        logging.info(str(e))
+        sys.exit()
+
+
+def cancel_all_orders(api):
+    logging.info("Cancelling all orders...")
+
+    try:
+        api.cancel_all_orders()
+        logging.info("All orders cancelled")
+    except Exception as e:
+        logging.error("Could not cancel all orders")
+        logging.error(e)
+        sys.exit()
+
+
+def is_asset_tradable(api, ticker):
+    try:
+        asset = api.get_asset(ticker)
+        if asset.tradable:
+            logging.info("Asset exists and is tradable")
+            return True
+        else:
+            logging.info("Asset exists but not tradable, exiting")
+            sys.exit()
+    except Exception as e:
+        logging.error("Asset does not exist or something happened!")
+        logging.error(e)
+        sys.exit()
+
+
+def main():
+    api = tradeapi.REST(
+        config.API_KEY, config.API_SECRET_KEY, config.API_URL, api_version="v2"
+    )
+
+    initialize_logging()
+
+    check_account_status(api)
+
+    cancel_all_orders(api)
+
+    ticker = "INSERT TICKER"
+
+    is_asset_tradable(api, ticker)
+
+    trader = Trader(ticker, api)
+
+    while True:
+        trading_success = trader.run(ticker)
+
+        if not trading_success:
+            logging.info("Trading was not successful, locking asset")
+            time.sleep(config.sleepTimeME)
+        else:
+            logging.info("Trading was successful!")
+            time.sleep(config.sleepTimeME)
+
 
 if __name__ == "__main__":
-    # Instantiate the trading bot
-    MyTrader = TraderBot(API_KEY, SECRET_KEY)
-
-    # Set the strategy for the trading bot
-    MyTrader.set_strategy(strategy)
-
-    # Run the trading bot
-    MyTrader.run()
+    main()
